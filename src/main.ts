2d982b094772a2294254a0ef4b3e74c85b7cec1d
mithelan <mithelandev@gmail.com>
2025-08-01 08:32:32 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { randomUUID } from 'crypto';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+    cors: true, 
+  });
   const PORT = process.env.PORT
  || 3006;
 
